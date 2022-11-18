6ccc3c71e3ed6211bc16f0768c0890d1318060b2
Seongtae <stae1102@gmail.com>
2022-11-18 15:12:24 +0900

    fix: Add async into databse module

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
+      useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
       }),
       inject: [ConfigService],
