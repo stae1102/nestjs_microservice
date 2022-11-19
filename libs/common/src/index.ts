c60a79d5dab86669582eab4e7d0718638717c9cb
Seongtae <stae1102@gmail.com>
2022-11-19 21:33:59 +0900

    chore: Add rmq module's path to lib index

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,3 +2,4 @@ export * from './database/abstract.repository';
 export * from './database/abstract.schema';
 export * from './database/database.module';
 export * from './rmq/rmq.service';
+export * from './rmq/rmq.module';
